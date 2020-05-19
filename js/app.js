3bfc98f9bd9010e85b15d53adacb6b409c49773c
Tomas K.T <tomas.konig.t@gmail.com>
2020-05-19 19:16:05 -0400

    Se agrega sistema de generación aleatoria

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var buttonGenerador = document.getElementById('hola')
 var bajar = document.getElementById('bajar')
+var rese = document.getElementById('reset')
+var colorBase = document.getElementById('coloresBase')
+var colorTodos = document.getElementById('coloresTodos')
+var dis = document.getElementById('formas')
+
 draw()
 
 buttonGenerador.addEventListener('click', draw)
 bajar.addEventListener('click', download)
+rese.addEventListener('click', randomCompleto)
+colorBase.addEventListener('click', randomColorBase)
+colorTodos.addEventListener('click', randomColor )
+dis.addEventListener('click', randomDis)
 
 function download() {
     var download = document.getElementById("download");
@@ -19,6 +28,7 @@ function draw() {
     var ciclo = document.getElementById('ciclo').value
     var anguloext = document.getElementById('rango1').value
     var colorf = document.getElementById("colorfondo").value;
+    
     ctx.clearRect(0,0, imagen.width, imagen.height);
     ctx.fillStyle = colorf;
     ctx.fillRect(0, 0, imagen.width, imagen.height);
@@ -26,6 +36,9 @@ function draw() {
 
     t.box = function box(ciclos,angulo) {
         var length = 10
+        //var grosor = document.getElementById("grosor").value
+        // this.pendWidth = grosor
+        //this.penWidth = grosor
         for (var i = 0; i < ciclos; i++) {
             primo(i + 1)
             this.forward(length)
@@ -54,6 +67,79 @@ function draw() {
 
     
     t.box(ciclo,anguloext)
-
+    
     // ctx.globalCompositeOperation = 'destination-over';
+}
+
+function randomCompleto(){
+    var iteraciones = genRand(1,800)
+    var angulos = genRand(1,180)
+
+    document.getElementById('ciclo').value = iteraciones
+    document.getElementById('cicloOut').value = iteraciones
+
+    document.getElementById('rango1').value = angulos
+    document.getElementById('rangoOut').value = angulos
+
+    document.getElementById('colorbase').value = color()
+
+    document.getElementById('colorprimo').value = color()
+
+    document.getElementById('colorfondo').value = color()
+
+    draw()
+}
+
+function randomColorBase(){
+    document.getElementById('colorbase').value = color()
+
+    document.getElementById('colorprimo').value = color()
+
+    draw()
+}
+
+function randomColor(){
+    document.getElementById('colorbase').value = color()
+
+    document.getElementById('colorprimo').value = color()
+
+    document.getElementById('colorfondo').value = color()
+
+    draw()
+}
+
+function randomDis(){
+    var iteraciones = genRand(1, 800)
+    var angulos = genRand(1, 180)
+    document.getElementById('ciclo').value = iteraciones
+    document.getElementById('cicloOut').value = iteraciones
+
+    document.getElementById('rango1').value = angulos
+    document.getElementById('rangoOut').value = angulos
+
+    draw()
+}
+
+function genRand(ini,limite){
+    var rand = 0
+    if (ini== 0){
+        return rand = Math.floor(Math.random() * limite)
+    }
+    else {
+        return rand = Math.floor(Math.random() * limite) + ini
+    }
+}
+
+function color(){
+    var red = genRand(0, 250)
+    var green = genRand(0, 250)
+    var blue = genRand(0, 250)
+
+    return "#" + componentToHex(red) + componentToHex(green) + componentToHex(blue)
+
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
